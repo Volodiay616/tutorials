724b022c2235d978ff7f906a60747d40724c769c
Volodiay616 <108336612+Volodiay616@users.noreply.github.com>
2023-01-25 12:03:56 +0400

    pre-commit 0.2

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
     "name": "Estate",
-    'version': '14.0.0.0.1',
+    "version": "14.0.0.0.1",
     "author": "Vladimir",
     "depends": ["base_setup", "sale"],
     "application": False,
